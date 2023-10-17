b43a580de68ce2d1b2b7e9b58caea862e1433245
Wagdi Saif <WagdiSaif121@gmail.com>
2023-10-17 10:53:36 -0700

    c++ tutorial

--- Cplusplusprograms/Cplusplusprograms/function.cpp
+++ Cplusplusprograms/Cplusplusprograms/function.cpp
@@ -5,78 +5,82 @@
 
 
 using namespace std;
-float  Sum(float x,float y) {
+float  Sum(float x, float y) {
 
 
 	return x + y;
 }
 
 template<class T>
-T functionChan(T x,T y) {
+T functionChan(T x, T y) {
 
 
 
 	return x > y ? x : y;
 
-	
+
 
 }
-typedef string (*info)(string);
-void  PrintName(info funct) {
-string arg=	funct("I am Argument of Function");
-cout << arg;
+typedef string(*info)(string);
+auto PrintName(info funct, string name) {
+	cout << "Name is :";
+
+	return	funct(name);
+
 
 }
 
 string ToString(string x) {
 
-	return "body of functions";
+
+	return x;
 }
 
 float  Max(float x, float y) {
 
-	
-	return x>y?x:y;//Equi to if(x>y) return x else return y;
+
+	return x>y ? x : y;//Equi to if(x>y) return x else return y;
 }
 void DisplayValue(float x, float y) {
-	cout << "x  value is:    " << x <<"\t y values is: "<<y <<endl;
+	cout << "x  value is:    " << x << "\t y values is: " << y << endl;
 }
-int mainFun()
+int main()
 {
-	
-		float x, y, ch;
-		cout << "\enter number one  : ";
-		cin >> x;
-		cout << "\enter number two  : ";
-		cin >> y;
-		DisplayValue(x, y);
-		//			cin >> a;
-	cout << "sumation numbers is " << Sum(x, y)<<endl;
+
+	float x, y, ch;
+	cout << "\enter number one  : ";
+	cin >> x;
+	cout << "\enter number two  : ";
+	cin >> y;
+	DisplayValue(x, y);
+	//			cin >> a;
+	cout << "sumation numbers is " << Sum(x, y) << endl;
 	cout << "Max number is " << Max(x, y) << endl;
-	PrintName([](string x) {
+	cout << PrintName([](string x) {
 
-		
+
 		return x;
-		
 
-	});
-	info str =&ToString;
+
+	}, "Wagdi Saif");
+	info str = &ToString;
 	cout << endl;
-	PrintName(str);
+	cout << PrintName(str, "Ahmed");
 	cout << endl;
-	auto fun = [](float x, float y)-> int  {
+	auto fun = [](float x, float y)-> int {
 		return x*y;
 
 	};
 	cout << "multiple number is:    \n" << fun(x, y) << endl;
-	int max1 =functionChan<int>(67, 22);
+	int max1 = functionChan<int>(67, 22);
 	double max2 = functionChan<double>(60.12, 22.12);
 	float max3 = functionChan<float>(67, 22);
 
 
-	
-		system("pause");
-	
-		return 0;}
+
+	system("pause");
+
+	return 0;
+}
 
 
